207944f4ca4593525871bec13fc507b7ee735da2
ShadyShawkat <shadyshawkat001@gmail.com>
2021-03-17 17:28:21 +0200

    Added account/role/order repos

---
+++ BL/Repositories/AccountRepository.cs
@@ -0,0 +1,40 @@
+﻿using BL.Bases;
+using DAL;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Repositories
+{
+    public class AccountRepository
+    {
+        ApplicationUserManager manager;
+
+        public AccountRepository(DbContext db)
+        {
+            manager = new ApplicationUserManager(db);
+        }
+
+        public ApplicationIdentityUser Find(string username, string password)
+        {
+            ApplicationIdentityUser result = manager.Find(username, password);
+            return result;
+        }
+
+        public IdentityResult Register(ApplicationIdentityUser user)
+        {
+            IdentityResult result = manager.Create(user, user.PasswordHash);
+            return result;
+        }
+
+        public IdentityResult AssignToRole(string userid, string rolename)
+        {
+            IdentityResult result = manager.AddToRole(userid, rolename);
+            return result;
+        }
+    }
+}
