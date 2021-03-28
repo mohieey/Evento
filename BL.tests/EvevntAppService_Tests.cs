be167f8de9cd3ceb946c3844eab95fca344a7576
Mohamed Mohiey <mohieey@users.noreply.github.com>
2021-03-28 09:12:40 +0200

    ModifiedTestClasses

---
+++ BL.tests/EvevntAppService_Tests.cs
@@ -0,0 +1,53 @@
+﻿using BL.AppServices;
+using BL.ViewModels;
+using DAL;
+using NUnit.Framework;
+using System;
+
+namespace BL.tests
+{
+    [TestFixture]
+    class EvevntAppService_Tests
+    {
+        public EventAppService eventAppService;
+        public AccountAppService accountAppService;
+        public ApplicationIdentityUser host;
+
+        [OneTimeSetUp]
+        public void EventAppServiceStartup()
+        {
+            accountAppService = new AccountAppService();
+            eventAppService = new EventAppService();
+            host = accountAppService.Find("TestHost", "TestHost");
+        }
+
+
+        [Test]
+        public void GetAllEventsById_Test()
+        {
+            int id = 1;
+            int eventId = eventAppService.GetEventById(id).ID;
+            Assert.That(eventId, Is.EqualTo(id));
+        }
+
+
+
+
+        [Test]
+        public void SaveNewEvent_Test()
+        {
+            EventViewModel eventViewModel = new EventViewModel();
+            eventViewModel.HostId = accountAppService.Find("TestHost","TestHost").Id;
+            eventViewModel.Name = "TestEvent";
+            eventViewModel.location = "Test";
+            eventViewModel.price = 4;
+            eventViewModel.TotalAvailableTickets = 5;
+            eventViewModel.category = 0;
+            eventViewModel.date = DateTime.Now;
+
+
+            bool result = eventAppService.SaveNewEvent(eventViewModel);
+            Assert.That(result, Is.EqualTo(true));
+        }
+    }
+}
--- BL.tests/ShoppingCartAppService_Tests.cs
+++ BL.tests/ShoppingCartAppService_Tests.cs
@@ -1,9 +1,7 @@
 ﻿using BL.AppServices;
-using BL.ViewModels;
 using DAL;
 using Microsoft.AspNet.Identity;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -39,53 +37,6 @@ namespace BL.tests
         {
             string result = shoppingCartAppService.GetShoppingCartByUserId(user.Id).ClientId;
             Assert.That(result, Is.EqualTo(user.Id));
-        }
-    }
-
-
-
-    [TestFixture]
-    class EvevntAppService_Tests
-    {
-        public EventAppService eventAppService;
-        public AccountAppService accountAppService;
-        public ApplicationIdentityUser host;
-
-        [OneTimeSetUp]
-        public void EventAppServiceStartup()
-        {
-            accountAppService = new AccountAppService();
-            eventAppService = new EventAppService();
-            host = accountAppService.Find("TestHost", "TestHost");
-        }
-
-
-        [Test]
-        public void GetAllEventsById_Test()
-        {
-            int id = 1;
-            int eventId = eventAppService.GetEventById(id).ID;
-            Assert.That(eventId, Is.EqualTo(id));
-        }
-
-
-
-
-        [Test]
-        public void SaveNewEvent_Test()
-        {
-            EventViewModel eventViewModel = new EventViewModel();
-            eventViewModel.HostId = "1634bca1-433c-4c78-8df5-06aea8ae71fc";
-            eventViewModel.Name = "TestEvent";
-            eventViewModel.location = "Test";
-            eventViewModel.price = 4;
-            eventViewModel.TotalAvailableTickets = 5;
-            eventViewModel.category = 0;
-            eventViewModel.date = DateTime.Now;
-
-
-            bool result = eventAppService.SaveNewEvent(eventViewModel);
-            Assert.That(result, Is.EqualTo(true));
         }
     }
 }
